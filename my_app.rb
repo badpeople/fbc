5c9f8e2d14b7b6405d297adcc6ba0cb569b0501d
Paul <brower.paul@gmail.com>
2011-05-14 15:59:33 -0400

    template

--- my_app.rb
+++ my_app.rb
@@ -4,50 +4,16 @@ require 'sinatra/base'
 class MyApp < Sinatra::Base
   set :static, true
   set :public, File.dirname(__FILE__) + '/static'
-  
+
   get '/' do
-    'Hello World from MyApp in separate file!'
+    @title = 'Home'
+    erb :index,:layout=>:application
   end
 
-  get '/template' do
-    @foo = 'erb'
-    erb :index
+  get '/contact' do
+    @title = 'Contact'
+    erb :contact,:layout=>:application
+
   end
 
-  get '/named_via_params/:argument' do
-    "
-Using: '/named_via_params/:argument'<br/>
-params[:argument] -> #{params[:argument]} (Try changing it)
-"
-  end
-
-  get '/named_via_block_parameter/:argument' do |argument|
-    "
-Using: '/named_via_block_parameter/:argument'<br/>
-argument -> #{argument}
-    "
-  end
-
-  get '/splat/*/bar/*' do
-    "
-Using: '/splat/*/bar/*'<br/>
-params[:splat] -> #{params[:splat].join(', ')}
-    "
-  end
-
-  get '/splat_extension/*.*' do
-    "
-Using: '/splat_extension/*.*'<br/>
-filename -> #{params[:splat][0]}<br/>
-extension -> #{params[:splat][1]}
-    "
-  end
-
-  get %r{/regexp_params_captures/([\w]+)} do
-    "params[:captures].first -> '#{params[:captures].first}'"
-  end
-
-  get %r{/regexp_captures_via_block_parameter/([\w]+)} do |c|
-    "c -> '#{c}'"
-  end
 end
---
+++ static/js/showhide.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+$('.col_sel .sel_bg').click(function() {
+$('.col_sel ul').slideToggle('slow', function() {
+});
+return false;
+});
+});
